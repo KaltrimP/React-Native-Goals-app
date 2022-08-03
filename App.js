71518049031d10add1b21a097ab4f1fc4abcee90
KaltrimP <kaltrimperzefaj@gmail.com>
2022-08-03 10:59:48 +0200

    initial sascha commit

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [myGoals, setMyGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setMyGoals((currentGoals) => [...currentGoals, enteredGoalText]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter goals!"
+          onChangeText={goalInputHandler}
+        ></TextInput>
+        <Button onPress={addGoalHandler} title="+ Goal"></Button>
+      </View>
+      <View>
+        <ScrollView>
+          {myGoals.map((goal) => (
+            <Text style={styles.goalItemStyle} key={goal}>
+              * {goal} *
+            </Text>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 20,
+    marginTop: 15,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "aqua",
+    width: "60%",
+    alignContent: "center",
+    paddingLeft: 20,
+  },
+  goalItemStyle: {
+    borderColor: "lightgreen",
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    justifyContent: "center",
+    marginTop: 5,
+    margin: 5,
+    padding: 5,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
+    textAlign: "center",
   },
 });
